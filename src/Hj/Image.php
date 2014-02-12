f45a1406842fe6e9312cdd712faea2aa3addcc0e
hjacquir <jacquirhatim@gmail.com>
2014-02-12 01:58:46 +0100

    add getWidth and getHeight into Image

--- src/Hj/Image.php
+++ src/Hj/Image.php
@@ -11,4 +11,38 @@ namespace Hj;
  */
 class Image extends File
 {
+    /**
+     * @var array
+     */
+    private $imageSize;
+    
+    /**
+     * @param string $filename The file name
+     */
+    public function __construct($filename)
+    {
+        parent::__construct($filename);
+        
+        $this->imageSize = getimagesize($filename);
+    }
+    
+    /**
+     * Return the width of the image
+     * 
+     * @return integer $width
+     */
+    public function getWidth()
+    {
+        return $this->imageSize[0];
+    }
+    
+    /**
+     * Return the height of the image
+     * 
+     * @return integer $height
+     */
+    public function getHeight()
+    {
+        return $this->imageSize[1];
+    }
 }
--- tests/Unit/ImageTest.php
+++ tests/Unit/ImageTest.php
@@ -16,10 +16,45 @@ require '../../vendor/autoload.php';
  */
 class ImageTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @var Image
+     */
+    private $image;
+    
+    public function setUp()
+    {
+        $this->image = new Image('fixtures/test.jpg');
+    }
+
     public function testImageIsAFile()
     {
-        $image = new Image('fixtures/test.png');
-        $this->assertTrue($image->isFile());
-        $this->assertInstanceOf('Hj\File', $image);
+        $this->assertTrue($this->image->isFile());
+        $this->assertInstanceOf('Hj\File', $this->image);
+    }
+    
+    public function testShouldHaveTheImageSizeAttribute()
+    {
+        $this->assertTrue($this->image->isFile());
+        $expectedImageSize = array(
+            0 => 441,
+            1 => 286,
+            2 => 2,
+            3 => 'width="441" height="286"',
+            'bits' => 8,
+            'channels' => 3,
+            'mime' => 'image/jpeg',
+        );
+        
+        $this->assertAttributeEquals($expectedImageSize, 'imageSize', $this->image); 
+    }
+    
+    public function testMethodGetWidthShouldReturnTheWidthOfImage()
+    {
+        $this->assertSame(441, $this->image->getWidth());
+    }
+    
+    public function testMethodGetHeightShouldReturnTheHeightOfImage()
+    {
+        $this->assertSame(286, $this->image->getHeight());
     }
 }
